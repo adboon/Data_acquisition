bbde5e8b8494f34ba06e51b698bf324dac2e071b
@adboon <a.d.boon@tudelft.nl>
2021-09-28 09:52:37 +0200

    ?

--- Programming/CheckingFilesWaves.py
+++ Programming/CheckingFilesWaves.py
@@ -6,11 +6,13 @@ from module_CheckWavefile import FindMaxValues
 
 import glob
 files = glob.glob("/home/anna/Documents/1Work_in_progress/WaveFilesIrregularFarkas/*.txt") #find all tdms files in a certain folder
+# files = glob.glob("/home/anna/Documents/1Work_in_progress/Programming/WaveFiles/*.txt") #find all tdms files in a certain folder
 files.sort()
 
-ratio_a_V = 0.0422 #4.22 mm/volt (should check with Farkas)
-
-
-maxS,maxV,maxA,masEta = FindMaxValues(files[0:4],ratio_a_V,1)
+Tp = 1.4 #Peak wave period [s] (needed for the transfer function)
+ratio_s_V = 0.0422 #4.22 mm/volt (should check with Farkas) Sleeptank value
+# ratio_s_V  = 0.0125#DUMMY VALUE 0.125 #0.125 m/volt 10V max = 125 mm max Flumetank value
+Uc = 0.5 #Current velocity [m/s]
+maxS,maxV,maxA,maxEta = FindMaxValues(files,Tp,Uc,ratio_s_V,1)
 
 # %%
